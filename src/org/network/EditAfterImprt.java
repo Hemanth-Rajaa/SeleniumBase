9f069885afe05c1aee94d3d2e25995d0f414bbed
R.Hemanthrajaa <hemanthrockz.rajaa@gmail.com>
2022-08-28 17:25:51 +0530

    edited for conflict

--- src/org/network/EditAfterImprt.java
+++ src/org/network/EditAfterImprt.java
@@ -3,8 +3,8 @@ package org.network;
 public class EditAfterImprt {
 
 	private void test() {
-		System.out.println("Edited on branch a");
-		System.out.println("Edit branch a 1");
+		System.out.println("(branch b edit for conflict), Edited on branch a");
+		System.out.println("Branch b edit for conflict, Edit branch a 1");
 		System.out.println("Edit on branch b");
 		System.out.println("Adding 2nd line on beranch B");
 		
