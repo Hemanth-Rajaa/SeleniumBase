55b915a3a75bb3c9577c2341879060dc99158fe4
R.Hemanthrajaa <hemanthrockz.rajaa@gmail.com>
2022-08-28 17:53:03 +0530

    creating conflict by branch a

--- src/org/network/EditAfterImprt.java
+++ src/org/network/EditAfterImprt.java
@@ -3,8 +3,9 @@ package org.network;
 public class EditAfterImprt {
 
 	private void test() {
-		System.out.println("Edited on branch a");
-		System.out.println("Edit branch a 1");
+		System.out.println("Edited on branch a");// editing branch a for conflict.
+		System.out.println("Edit branch a 1");// conflict did not happen by branch b
+		System.out.println("adding few more lines for conflict");
 		
 	}
 }
